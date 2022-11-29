78e17577da4ac5b3cc8412d3c571fe5916f2c042
L1oid <lloidtm@gmail.com>
2022-11-29 22:25:01 +0700

    add interceptor

--- CompLab5.bat
+++ CompLab5.bat
@@ -11,6 +11,7 @@ javac.exe Lab5\WEB-INF\classes\pack\builder\user\UserBuilder.java -classpath ".\
 javac.exe Lab5\WEB-INF\classes\pack\repository\user\*.java -classpath ".\Lab5\WEB-INF\classes;C:\Users\lloid\Study\Portable\GlassFish\glassfish\lib\javaee.jar;"
 javac.exe Lab5\WEB-INF\classes\pack\repository\product\*.java -classpath ".\Lab5\WEB-INF\classes;C:\Users\lloid\Study\Portable\GlassFish\glassfish\lib\javaee.jar;"
 
+javac.exe Lab5\WEB-INF\classes\pack\controller\interceptor\*.java -classpath ".\Lab5\WEB-INF\classes;C:\Users\lloid\Study\Portable\GlassFish\glassfish\lib\javaee.jar;"
 javac.exe Lab5\WEB-INF\classes\pack\controller\config\*.java -classpath ".\Lab5\WEB-INF\classes;C:\Users\lloid\Study\Portable\GlassFish\glassfish\lib\javaee.jar;"
 javac.exe Lab5\WEB-INF\classes\pack\controller\dto\*.java -classpath ".\Lab5\WEB-INF\classes;C:\Users\lloid\Study\Portable\GlassFish\glassfish\lib\javaee.jar;"
 javac.exe Lab5\WEB-INF\classes\pack\controller\path\product\*.java -classpath ".\Lab5\WEB-INF\classes;C:\Users\lloid\Study\Portable\GlassFish\glassfish\lib\javaee.jar;"
--- Lab5/WEB-INF/classes/pack/controller/path/product/ProductController.java
+++ Lab5/WEB-INF/classes/pack/controller/path/product/ProductController.java
@@ -6,8 +6,10 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.json.bind.JsonbException;
@@ -18,29 +20,30 @@ import java.util.ArrayList;
 import pack.builder.Built;
 import pack.model.dto.Product;
 import pack.model.interfaces.product.IProductModel;
+import pack.controller.interceptor.IdRequired;
 import pack.controller.dto.Token;
 import pack.controller.tools.TokenTools;
 
 @Path("/products")
 public class ProductController {
+    @Context
+    ContainerRequestContext requestContext;
+
     @Inject @Built
     IProductModel model;
 
     @GET
+    @IdRequired
     @Path("/list")
     @Produces("application/json")
     public Response getProductList(@HeaderParam("User-token") String userToken) {
-        Token token;
         Jsonb jsonb = JsonbBuilder.create();
         String resultJSON = jsonb.toJson("undefinedError");
         try {
-            token = jsonb.fromJson(userToken, new Token(){}.getClass().getGenericSuperclass());
-            if (TokenTools.verifyToken(token)) {
-                resultJSON = jsonb.toJson(model.getProductsList());
-                if (resultJSON == null) {
-                    return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(jsonb.toJson("Unavailable DataBase Connection")).build();
-                } else return Response.ok(resultJSON).build();
-            } else return Response.status(Response.Status.UNAUTHORIZED).entity(jsonb.toJson("ExpiredToken")).build();
+            resultJSON = jsonb.toJson(model.getProductsList());
+            if (resultJSON == null) {
+                return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(jsonb.toJson("Unavailable DataBase Connection")).build();
+            } else return Response.ok(resultJSON).build();
         } catch (JsonbException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(jsonb.toJson(e.getMessage())).build();	             
         } catch (Exception e) {
@@ -49,24 +52,21 @@ public class ProductController {
     }
 
     @POST
+    @IdRequired
     @Path("/add")
     @Consumes("application/json")
     @Produces("application/json")
     public Response addProduct(@HeaderParam("User-token") String userToken, String newProduct) {
-        Token token;
         Product product;
         Jsonb jsonb = JsonbBuilder.create();
         String resultJSON = jsonb.toJson("undefinedError");
         try {
-            token = jsonb.fromJson(userToken, new Token(){}.getClass().getGenericSuperclass());
             product = jsonb.fromJson(newProduct, new Product(){}.getClass().getGenericSuperclass());
-            if (TokenTools.verifyToken(token)) {
-                Boolean status = model.addProduct(product);
-                if (status == true) {
-                    resultJSON = jsonb.toJson("productAdded");
-                    return Response.ok(resultJSON).build(); 
-                } else return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity("Unavailable DataBase Connection").build();
-            } else return Response.status(Response.Status.UNAUTHORIZED).entity("ExpiredToken").build();
+            Boolean status = model.addProduct(product);
+            if (status == true) {
+                resultJSON = jsonb.toJson("productAdded");
+                return Response.ok(resultJSON).build(); 
+            } else return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity("Unavailable DataBase Connection").build();
         } catch (JsonbException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();	             
         } catch (Exception e) {
@@ -75,23 +75,20 @@ public class ProductController {
     }
 
     @DELETE
+    @IdRequired
     @Path("/delete")
     @Produces("application/json")
     public Response deleteProducts(@HeaderParam("User-token") String userToken,@HeaderParam("Delete-row") int toDelete) {
-        Token token;
         Jsonb jsonb = JsonbBuilder.create();
         String resultJSON = jsonb.toJson("undefinedError");
         try {
-            token = jsonb.fromJson(userToken, new Token(){}.getClass().getGenericSuperclass());
-            if (TokenTools.verifyToken(token)) {
-                Boolean status = model.deleteProduct(toDelete);
-                if(status == null) {
-                    return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity("Unavailable DataBase Connection").build();
-                } else {
-                    resultJSON = jsonb.toJson(status);
-                    return Response.ok(resultJSON).build(); 
-                }
-            } else return Response.status(Response.Status.UNAUTHORIZED).entity("ExpiredToken").build();
+            Boolean status = model.deleteProduct(toDelete);
+            if(status == null) {
+                return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity("Unavailable DataBase Connection").build();
+            } else {
+                resultJSON = jsonb.toJson(status);
+                return Response.ok(resultJSON).build(); 
+            }
         } catch (JsonbException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();	             
         } catch (Exception e) {
